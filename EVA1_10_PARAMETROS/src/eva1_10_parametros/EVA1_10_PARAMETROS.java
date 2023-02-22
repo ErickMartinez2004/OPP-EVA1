b54901017652ebd8d237ba8982df8992c3b53866
ErickMartinez2004 <124701911+ErickMartinez2004@users.noreply.github.com>
2023-02-22 09:22:59 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/eva1_10_parametros/EVA1_10_PARAMETROS.java
@@ -0,0 +1,17 @@
+
+package eva1_10_parametros;
+
+
+public class EVA1_10_PARAMETROS {
+
+    
+    
+    public static void main(String[] args) {
+      nPrintLn ("HOLA",5);
+    }
+     public static void nPrintLn (String message, int n ){
+           for (int i = 0; i < n; i++) {
+               System.out.println(message);
+           }
+       }
+}
