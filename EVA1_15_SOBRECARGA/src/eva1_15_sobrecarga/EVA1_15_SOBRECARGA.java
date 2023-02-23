b7f3cb89a64896e7cb96fc26259324c3d2ab7e1f
ErickMartinez2004 <124701911+ErickMartinez2004@users.noreply.github.com>
2023-02-23 16:40:06 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,16 @@
+package eva1_15_sobrecarga;
+
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        /*DIFERENTES PRINTLN : SON 10 DIFERENTES 
+        METODOS, PARA IMPRIMIR DIFERENTES TIPOS DE DATOS*/
+ /*PARA CREAR SOBRE CARGA DE MÉTODOS 
+        LA FIRMA DEL MÉTODO DEBE SER UNICA*/
+        System.out.println("HOLA MUNDO");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+
+}
